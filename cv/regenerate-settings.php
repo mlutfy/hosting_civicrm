36d7c0e8847c909771a5a61ce1a3b3f73eb8f9bb
Mathieu Lu <mathieu@symbiotic.coop>
2024-10-01 21:30:50 +0200

    cv/regenerate-settings.php: yet another rewrite to avoid bootstrapping cv/civicrm completely (more fixes for Drupal9/10)

--- cv/regenerate-settings.php
+++ cv/regenerate-settings.php
@@ -28,6 +28,9 @@ $config = [
   // Optional values: set defaults
   'regen_keys' => FALSE,
   'site_key' => FALSE,
+  'sign_keys' => FALSE,
+  'cred_keys' => FALSE,
+  'deploy_id' => FALSE,
   'templates_c' => FALSE,
 ];
 
@@ -54,7 +57,7 @@ if (!file_exists($settingsPath)) {
 if (in_array($config['cms'], ['Drupal', 'Drupal8'])) {
   $config['templates_c'] = getcwd() . '/private/files/civicrm/templates_c';
 }
-elseif ($model->cms == 'WordPress') {
+elseif ($config['cms'] == 'WordPress') {
   $config['templates_c'] = getcwd() . '/wp-content/uploads/civicrm/templates_c';
 }
 else {
@@ -76,24 +79,30 @@ if (file_exists($settingsPath)) {
       if (is_array($token)) {
         if ($token[0] == T_WHITESPACE || $token[0] == T_COMMENT || $token[0] == T_DOC_COMMENT) {
           // do nothing
-        } else if ($token[0] == T_STRING && strtolower($token[1]) == 'define') {
+        } elseif ($token[0] == T_STRING && strtolower($token[1]) == 'define') {
             $state = 1;
-        } else if ($state == 2 && is_constant($token[0])) {
+        } elseif ($state == 2 && is_constant($token[0])) {
             $key = $token[1];
             $state = 3;
-        } else if ($state == 4 && is_constant($token[0])) {
+        } elseif ($state == 4 && is_constant($token[0])) {
             $value = $token[1];
             $state = 5;
         }
       } else {
         $symbol = trim($token);
         if ($symbol == '(' && $state == 1) {
-            $state = 2;
-        } else if ($symbol == ',' && $state == 3) {
-            $state = 4;
-        } else if ($symbol == ')' && $state == 5) {
-            $defines[strip($key)] = strip($value);
-            $state = 0;
+          $state = 2;
+        } elseif ($symbol == ',' && $state == 3) {
+          $state = 4;
+        } elseif ($symbol == ')' && $state == 5) {
+          $value = strip($value);
+          $key = strip($key);
+	  // Ignore any value such as %%deployID%% or a constant defined by
+          // another constant (ex: setting CIVICRM_SIGN_KEYS = _CIVICRM_SIGN_KEYS)
+          if ($value && substr($value, 0, 2) == '%%' && substr($value, -2, 2) == '%%' && $key != substr($value, 1)) {
+            $defines[$key] = $value;
+          }
+          $state = 0;
         }
       }
       $token = next($tokens);
@@ -148,155 +157,84 @@ $corePath = $config['civicrm_root'];
 // Grab Drush relevant variables
 require_once getcwd() . '/drushrc.php';
 
-// Ensure that https URLs are generated, especially on WordPress
-$_SERVER['HTTPS'] = 'on';
-
-// Civi\Setup fails because of bootstrap issues on Drupal9+
-// but for now, leaving the others on level=classloader because it is
-// less likely to crash if some values are incorrectly set
-if ($config['cms'] == 'Drupal8') {
-  eval(`cv php:boot --level=cms-full`);
-}
-else {
-  eval(`cv php:boot --level=classloader`);
-}
-
-\Civi\Setup::assertProtocolCompatibility(1.0);
-\Civi\Setup::init([
-  // This is just enough information to get going. *.civi-setup.php does more scanning.
-  'cms' => $config['cms'],
-  // This should not be necessary but otherwise we get very weird results
-  // such as: http://crm.example.org/usr/local/bin/usr/local/bin/aegir
-  // c.f. civicrm-core/setup/plugins/init/Drupal8.civi-setup.php
-  'cmsBaseUrl' => $config['host'],
-  'srcPath' => $corePath,
-]);
-
 if (empty($_SERVER['db_user']) || empty($_SERVER['db_passwd'])) {
   throw new Exception("Missing database credentials such as db_user or db_passwd.");
 }
 
-// init() made the initial guess. Now we can overwrite with user-supplied data.
-$setup = \Civi\Setup::instance();
-/**
- * @var \Civi\Setup\Model $model
- */
-$model = $setup->getModel();
-$model->cmsDb = [
-  'server' => $_SERVER['db_host'] . ':' . $_SERVER['db_port'],
-  'username' => $_SERVER['db_user'],
-  'password' => $_SERVER['db_passwd'],
-  'database' => $_SERVER['db_name'],
-  'dbSSL' => '', // Need to set if relevant later
-  'CMSdbSSL' => '', // Need to set if relevant later
-];
-$model->db = [
-  'server' => $_SERVER['db_host'] . ':' . $_SERVER['db_port'],
-  'username' => $_SERVER['db_user'],
-  'password' => $_SERVER['db_passwd'],
-  'database' => $_SERVER['db_name'],
-  'dbSSL' => '', // Need to set if relevant later
-  'CMSdbSSL' => '', // Need to set if relevant later
-];
-// if ($lang) {
-//  $model->lang = $lang;
-//}
-
-// Define imported values
-if (!$config['regen_keys']) {
-  $model->credKeys = [$defines['_CIVICRM_CRED_KEYS'] ?? $defines['CIVICRM_CRED_KEYS']];
-  $model->deployID = $defines['_CIVICRM_DEPLOY_ID'] ?? $defines['CIVICRM_DEPLOY_ID'];
-  $model->siteKey = $config['site_key'] ?: $defines['CIVICRM_SITE_KEY'];
-  $model->signKeys = [$defines['_CIVICRM_SIGN_KEYS'] ?? $defines['CIVICRM_SIGN_KEYS']];
-}
-$model->cms = $config['cms'];
-$model->cmsBaseUrl = $config['host'];
-$model->templateCompilePath = $config['templates_c'];
-
-// This might not always be necessary, but it was done before for Drupal7
-// without this, on d7, it will default to "private/files/civicrm"
-// ex: cv ev 'echo \Civi::paths()->getPath("[civicrm.files]/");'
-$model->paths = [
-  'civicrm.files' => [
-    'path' => getcwd() . '/files/civicrm',
-  ]
-];
+// Helper
+$toAlphanum = function($bits) {
+  return preg_replace(';[^a-zA-Z0-9];', '', base64_encode($bits));
+};
 
 // Setup CiviCRM settings if not set
 if ($config['regen_keys']) {
-  // Generate all the relevant variables
-  $toAlphanum = function($bits) {
-    return preg_replace(';[^a-zA-Z0-9];', '', base64_encode($bits));
-  };
-
-  // Setup Cred Keys
-  if (empty($model->credKeys)) {
-    $model->credKeys = ['aes-cbc:hkdf-sha256:' . $toAlphanum(random_bytes(37))];
-  }
-  if (is_string($model->credKeys)) {
-    $model->credKeys = [$model->credKeys];
-  }
-  // Setup Deploy Key
-  if (empty($model->deployID)) {
-    $model->deployID = $toAlphanum(random_bytes(10));
-  }
-  // Setup Site Key
-  if (!empty($model->siteKey)) {
-      // skip
-  }
-  elseif (function_exists('random_bytes')) {
-    $model->siteKey = $toAlphanum(random_bytes(32));
-  }
-  elseif (function_exists('openssl_random_pseudo_bytes')) {
-    $model->siteKey = $toAlphanum(openssl_random_pseudo_bytes(32));
-  }
-  else {
-    throw new \RuntimeException("Failed to generate a random site key");
-  }
-  //Setup Sign Key
-  if (empty($model->signKeys)) {
-    $model->signKeys = ['jwt-hs256:hkdf-sha256:' . $toAlphanum(random_bytes(40))];
-    // toAlpanum() occasionally loses a few bits of entropy, but random_bytes() has significant excess, so it's still more than ample for 256 bit hkdf.
-  }
-  if (is_string($model->signKeys)) {
-    $model->signKeys = [$model->signKeys];
-  }
+  $config['cred_keys'] = 'aes-cbc:hkdf-sha256:' . $toAlphanum(random_bytes(37));
+  $config['deploy_id'] = $toAlphanum(random_bytes(10));
+  $config['site_key'] = $toAlphanum(random_bytes(32));
+  $config['sign_keys'] = 'jwt-hs256:hkdf-sha256:' . $toAlphanum(random_bytes(40));
 }
 
-// Build params
-$params = \Civi\Setup\SettingsUtil::createParams($model);
+// Check for any missing variable
+$config['cred_keys'] = $config['cred_keys'] ?: $defines['_CIVICRM_CRED_KEYS'] ?? $defines['CIVICRM_CRED_KEYS'] ?? 'aes-cbc:hkdf-sha256:' . $toAlphanum(random_bytes(37));
+$config['deploy_id'] = $config['deploy_id'] ?: $defines['_CIVICRM_DEPLOY_ID'] ?? $defines['CIVICRM_DEPLOY_ID'] ?? $toAlphanum(random_bytes(10));
+$config['site_key'] = $config['site_key'] ?: $defines['_CIVICRM_SITE_KEY'] ?? $defines['CIVICRM_SITE_KEY'] ?? $toAlphanum(random_bytes(32));
+$config['sign_keys'] = $config['sign_keys'] ?: $defines['_CIVICRM_SIGN_KEYS'] ?? $defines['CIVICRM_SIGN_KEYS'] ?? 'jwt-hs256:hkdf-sha256:' . $toAlphanum(random_bytes(40));
 
-// Regenerate TPL file and output
-$tplPath = implode(DIRECTORY_SEPARATOR,
-  [$model->srcPath, 'templates', 'CRM', 'common', 'civicrm.settings.php.template']
-);
-$str = \Civi\Setup\SettingsUtil::evaluate($tplPath, $params);
+$settingstpl = file_get_contents($config['civicrm_root'] . '/templates/CRM/common/civicrm.settings.php.template');
 
-// Find Clean URLs section
-$pos = strpos($str, "if (!defined('CIVICRM_CLEANURL'))");
-if ($pos !== FALSE) {
-  $str = substr($str, 0, $pos)
-    . "// Added by Aegir: Force Clean URLs to prevent bad Civi multilingual urls\ndefine('CIVICRM_CLEANURL', 1 );\n\n"
-    . substr($str, $pos, strlen($str));
+$tokens = [
+  'cms' => $config['cms'],
+  'CMSdbUser' => $_SERVER['db_user'],
+  'CMSdbPass' => $_SERVER['db_passwd'],
+  'CMSdbHost' => $_SERVER['db_host'] . ':' . $_SERVER['db_port'],
+  'CMSdbName' => $_SERVER['db_name'],
+  // @todo
+  'CMSdbSSL' => '',
+  'dbUser' => $_SERVER['db_user'],
+  'dbPass' => $_SERVER['db_passwd'],
+  'dbHost' => $_SERVER['db_host'] . ':' . $_SERVER['db_port'],
+  'dbName' => $_SERVER['db_name'],
+  // @todo
+  'dbSSL' => '',
+  'extraSettings' => '',
+  'crmRoot' => $config['civicrm_root'],
+  'templateCompileDir' => $config['templates_c'],
+  'baseURL' => $config['host'],
+  'siteKey' => $config['site_key'],
+  'credKeys' => $config['cred_keys'],
+  'deployID' => $config['deploy_id'],
+  'signKeys' => $config['sign_keys'],
+  'templateCompileDir' => $config['templates_c'],
+];
+
+$extraSettings = [
+  'Additional settings generated by hosting_civicrm',
+];
+
+if (in_array($config['cms'], ['Drupal', 'Drupal8'])) {
+  $extraSettings[] = '$civicrm_paths[\'civicrm.files\'][\'url\'] = \'' . $config['host'] . '/sites/' . $config['host'] . '/files/civicrm\';';
+  $extraSettings[] = '$civicrm_paths[\'civicrm.files\'][\'path\'] = \'' . getcwd() . '/files/civicrm\';';
+  $extraSettings[] = '$civicrm_paths[\'civicrm.private\'][\'path\'] = \'' . getcwd() . '/private/files/civicrm\';';
 }
-else {
-  echo 'Could not set CLEAN URL variable';
-}
+
+// Always enable clean URLs
+$extraSettings[] = '// Force Clean URLs to prevent bad Civi multilingual urls';
+$extraSettings[] = 'define(\'CIVICRM_CLEANURL\', 1);';
 
 // On WordPress, include the drushrc.php for the WP salts
-if ($model->cms == 'WordPress') {
-  $pos = strpos($str, '// Additional settings generated by installer:');
-  if ($pos !== FALSE) {
-    $str = substr($str, 0, $pos)
-      . "// Added by Aegir: ensures we have the WP salts loaded\n@include_once('" . getcwd() . "/drushrc.php');\n\n"
-      . substr($str, $pos, strlen($str));
-  }
-  else {
-    echo "Could not add the drushrc.php include\n";
-  }
+if ($config['cms'] == 'WordPress') {
+  $extraSettings[] = '// Ensure we have the WP salts loaded';
+  $extraSettings[] = '@include_once(\'' . getcwd() . '/drushrc.php\');';
 }
 
-// Output the file
+$tokens['extraSettings'] = implode("\n", $extraSettings);
+
+// Interpolate tokens
+foreach ($tokens as $token => $value) {
+  $settingstpl = str_replace('%%' . $token . '%%', $value, $settingstpl);
+}
+
+// Write the file
 chmod($settingsPath, 0640);
-file_put_contents($settingsPath, $str);
+file_put_contents($settingsPath, $settingstpl);
 chmod($settingsPath, 0440);
