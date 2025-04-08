20ded01e146471cf3ae8ae2324f68468c526c10a
Mathieu Lu <mathieu@symbiotic.coop>
2025-04-08 19:13:51 +0200

    Add support for sitekey/credkey when regenerating civicrm.settings.php

--- drush/Provision/Service/civicrm.php
+++ drush/Provision/Service/civicrm.php
@@ -11,6 +11,9 @@ class Provision_Service_civicrm extends Provision_Service {
    */
   static function subscribe_site($context) {
     $context->setProperty('civicrm_sitekey');
+    $context->setProperty('civicrm_ansible_sitekey');
+    $context->setProperty('civicrm_ansible_credkey');
+    $context->setProperty('civicrm_ansible_signkey');
 
     // Not used?
     $context->setProperty('civicrm_version');
